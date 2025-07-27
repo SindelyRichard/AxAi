924b8332e69a1010c3daa72a3d44b613573082da
SindelyRichard <rsindely@gmail.com>
2025-07-27 17:54:54 +0200

    feat:App service:add,delete,run

--- src/main/java/com/axai/axai/repository/AppRepository.java
+++ src/main/java/com/axai/axai/repository/AppRepository.java
@@ -3,7 +3,9 @@ package com.axai.axai.repository;
 import com.axai.axai.entities.App;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface AppRepository extends JpaRepository<App, UUID> {
+    boolean existsByName(String name);
 }
--- src/main/java/com/axai/axai/service/AppService.java
+++ src/main/java/com/axai/axai/service/AppService.java
@@ -1,11 +1,37 @@
 package com.axai.axai.service;
 
+import com.axai.axai.entities.App;
 import com.axai.axai.repository.AppRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Service
 public class AppService {
     private final AppRepository appRepository;
+
+    public App addApp(String name){
+        if(appRepository.existsByName(name)){
+            throw new RuntimeException("App already exists!");
+        }
+        App app = new App();
+        app.setName(name);
+
+        return appRepository.save(app);
+    }
+
+    public void deleteApp(UUID appId){
+        if(!appRepository.existsById(appId)){
+            throw new RuntimeException("App not exists!");
+        }
+        appRepository.deleteById(appId);
+    }
+
+    public void runApp(UUID appId){
+        App app =  appRepository.findById(appId).orElseThrow(()->new RuntimeException("App not exists!"));
+
+        System.out.println(app.getName()+" app is running...");
+    }
 }
